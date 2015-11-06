7781e07babdb34b0ffbe049022c736ca7036581d
Joanna De Los Santos <jdelossa93@gmail.com>
2015-11-06 17:41:21 -0500

    changes

--- templates/content.php
+++ templates/content.php
@@ -1,9 +1,6 @@
-<article <?php post_class(); ?>>
-  <header>
-    <h2 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h2>
-    <?php get_template_part('templates/entry-meta'); ?>
-  </header>
-  <div class="entry-summary">
-    <?php the_excerpt(); ?>
+<div <?php post_class(); ?>>
+  <div class="col-md-4 single-profile">
+    <a href="<?php the_permalink(); ?>" class="doctor-portrait"><?php the_post_thumbnail( 'full' ); ?></a>
+    <h5 class="entry-title"><a href="<?php the_permalink(); ?>"><?php the_title(); ?></a></h5>
   </div>
-</article>
+</div>
--- templates/header.php
+++ templates/header.php
@@ -2,8 +2,8 @@
     <nav class="navbar-fixed-top">
       <div class="header-contact">
         <div class="container">
-        <a href="#" class="pull-right"><i class="fa fa-phone"></i> <?php echo (get_option('qs_contact_phone')); ?></a>
-        <a href="#" class="pull-right"><i class="fa fa-map-marker"></i> <?php echo (get_option('qs_contact_street')); ?>, <?php echo (get_option('qs_contact_city')); ?>, <?php echo (get_option('qs_contact_state')); ?> <?php echo (get_option('qs_contact_zip')); ?></a>
+        <a href="tel:<?php echo (get_option('qs_contact_phone')); ?>" class="pull-right"><i class="fa fa-phone"></i><span class="phone"> <?php echo (get_option('qs_contact_phone')); ?></span></a>
+        <a href="#" class="pull-right"><i class="fa fa-map-marker"></i><span class="map"> <?php echo (get_option('qs_contact_street')); ?>, <?php echo (get_option('qs_contact_city')); ?>, <?php echo (get_option('qs_contact_state')); ?> <?php echo (get_option('qs_contact_zip')); ?></span></a>
         </div>
       </div>
       <div class="nav-primary">
