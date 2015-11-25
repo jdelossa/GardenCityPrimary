ee448c493de107b57a5cd26d443e8da62419a722
Joanna De Los Santos <jdelossa93@gmail.com>
2015-11-25 15:36:25 -0500

    changes

--- templates/footer.php
+++ templates/footer.php
@@ -5,7 +5,7 @@
     <div class="logo">
       <img src="/gardencityprimary/wp-content/uploads/2015/11/WUH_Affiliate_Color-O1.png" alt="Winthrop Logo">
     </div>
-    <p class="text-center small">Questions? We are here to help | <a href="http://localhost:3000/gardencityprimary/contact-us/">Contact Garden City Primary Medical Care</a></p>
+    <p class="text-center questions">Questions? We are here to help | <a href="http://localhost:3000/gardencityprimary/contact-us/">Contact Us</a></p>
     <p class="text-center"><span class="privacy small"><a href="http://localhost:3000/gardencityprimary/patients/patient-privacy-policy/">Privacy Policy</a></span> | <i class="fa fa-copyright"></i> <?php echo date('Y'); ?> - <?php bloginfo('name'); ?></p>
   </div>
 </footer>
